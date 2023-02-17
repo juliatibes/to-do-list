4525c01c2caa8951fa4bd8694f7defca796133d7
Júlia Tibes <julia.tibes@gmail.com>
2023-02-17 09:18:14 -0300

    funcao limpar tarefas adicionada

--- listaTarefas.js
+++ listaTarefas.js
@@ -1,13 +1,15 @@
 const listaTarefas = document.querySelector('#listaTarefas')
 const caixaTexto = document.querySelector('#caixaDeTexto')
 const botaoAdicionar = document.querySelector('#botaoAdicionar')
+const listaSuspensa = document.querySelector('#listaSuspensa')
 
 //Adiciona um novo li + texto da tarefa
 botaoAdicionar.addEventListener('click', function(){
     const textoDaTarefa = caixaTexto.value
     caixaTexto.value = ''
 
-    listaTarefas.appendChild(adicionaTarefa(textoDaTarefa))
+    listaTarefas.appendChild(adicionaTarefa(textoDaTarefa));
+    exibeOcultaListaSuspensa();
     caixaTexto.focus();
 })
 
@@ -40,13 +42,38 @@ function adicionaBotaoRemover(){
     const botaoRemover = document.createElement('button')
     botaoRemover.textContent = '✗'
     botaoRemover.className = 'remover'
-    console.log(listaTarefas)
 
     //Remove li da tarefa
     botaoRemover.addEventListener('click', function(){
         listaTarefas.removeChild(this.parentNode)
+        exibeOcultaListaSuspensa()
     })
 
     return botaoRemover
 }
+
+function exibeOcultaListaSuspensa(){
+    const elementSpan = document.querySelector('#tarefa')
+
+    if(elementSpan === null) {
+       listaSuspensa.setAttribute('hidden', 'hidden') 
+    } else {
+        listaSuspensa.removeAttribute('hidden')
+    }
+
+}
+
+listaSuspensa.addEventListener('change', function(){
+    if(listaSuspensa.selectedIndex === 1 || listaSuspensa.selectedIndex === 2) {
+        const vetorTarefas = listaTarefas.querySelectorAll('#tarefa')
+        for(tarefa of vetorTarefas) {
+            tarefa.dispatchEvent(new Event('click'));
+        }
+    } else if (listaSuspensa.selectedIndex === 3){
+        const vetorBotoes = listaTarefas.querySelectorAll('.remover')
+        for(botao of vetorBotoes) {
+            botao.dispatchEvent(new Event('click'));
+        }
+    }
+})
 
